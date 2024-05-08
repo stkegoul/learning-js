b722af851c7291c513fcf898eb805b9df0cade7e
Praise Philemon <praisephilemon@gmail.com>
2024-05-08 18:53:58 +0100

    Completed exercise 30

--- js-daily-practice/basic.js
+++ js-daily-practice/basic.js
@@ -440,3 +440,19 @@
         }
 
     }
+
+// 30 - Check whether a string "Script" appears at the 5th (index 4) position in a given string. If "Script" appears in the string, return the string without "Script" otherwise return the original one.
+
+    const stringManipulation7 = (str) => {
+
+        if (str.length > 6) {
+
+            if (str.substring(4,10) === "script") {
+                console.log(str.slice(0, 4) + str.slice(10, str.length))
+            } else {
+                console.log(str)
+            }
+        } else {
+            console.log(str)
+        }
+    }
